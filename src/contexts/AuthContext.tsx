9c5d76e441a7784e994d28bc9fba1a8d3dda95cf
Matheus97px <matheus97p.q@gmail.com>
2025-09-15 18:47:25 -0300

    feat: autenticação usuario

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,61 @@
+import { createContext, useState, type ReactNode } from "react";
+import type UserLogin from "../models/UserLogin";
+import { login } from "../services/Service";
+
+
+
+interface AuthContextProps {
+    user: UserLogin;
+    handleLogout(): void;
+    handleLogin(userLogin: UserLogin): void;
+    isLoading: boolean;
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export function AuthProvider({children} : AuthProviderProps) {
+
+    const [user, setUser] = useState<UserLogin>({
+        id: 0,
+        name: '',
+        username: '',
+        password: '',
+        photoUrl: '',
+        token: ''
+    });
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    async function handleLogin(userLogin:UserLogin) {
+        setIsLoading(true);
+        try{
+            await login('/usuarios/logar', userLogin, setUser);
+            ToastAlerta('Login realizado com sucesso', 'success');
+        } catch (error) {
+            ToastAlerta('Dados do usuário inválidos', 'error');
+        }
+        setIsLoading(false);
+    }
+
+    function handleLogout() {
+        setUser({
+            id: 0,
+            name: '',
+            username: '',
+            password: '',
+            photoUrl: '',
+            token: ''
+        });
+    }
+
+    return (
+        <AuthContext.Provider value={{user, handleLogin, handleLogout, isLoading}}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
---
+++ src/models/UserLogin.ts
@@ -0,0 +1,8 @@
+export default interface UserLogin {
+    id: number;
+    name: string;
+    username: string;
+    password: string;
+    photoUrl: string;
+    token: string;
+}
