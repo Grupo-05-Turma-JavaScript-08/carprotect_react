306c6c16c8bc892c21f188d7341215d56c72a2d5
gmartins9042 <gmartins9042@gmail.com>
2025-09-15 20:24:26 -0300

    feat: page contact estilização

--- src/pages/contato/Contato.tsx
+++ src/pages/contato/Contato.tsx
@@ -5,13 +5,13 @@ function Contato() {
         <>
             <div className="grid grid-rows-2 gap-10 bg-sky-900">
 
-                <div className="grid grid-cols-2 items-center rounded-2xl m-20 my-auto bg-slate-300 ">
+                <div className="grid grid-cols-2 items-center rounded-2xl m-20 my-auto">
                     <div className="flex justify-center md:justify-start">
                         <div className="flex flex-col gap-20 justify-center items-center ">
-                            <h1 className="text-7xl font-extrabold text-black">
+                            <h1 className="text-7xl font-extrabold text-white">
                                 Fale Conosco
                             </h1>
-                            <p className="text-black text-2xl font-semibold w-3/4 text-center">Tem alguma dúvida, precisa de uma cotação ou quer saber mais sobre nossos serviços?
+                            <p className="text-white text-2xl font-semibold w-3/4 text-start">Tem alguma dúvida, precisa de uma cotação ou quer saber mais sobre nossos serviços?
 
                                 Estamos à disposição para te ajudar! Entre em contato conosco através do formulário abaixo ou pelos nossos canais de atendimento. Nossa equipe está pronta para oferecer o melhor suporte e as soluções ideais para suas necessidades de seguro.
 
@@ -23,7 +23,7 @@ function Contato() {
                         <img
                             src="https://i.ibb.co/RpPCSchv/Imagem-do-Whats-App-de-2025-09-14-as-13-41-39-ba89d92a.jpg"
                             alt="Imagem família sorrindo no carro"
-                            className=" rounded-r-2xl h-160 w-auto"
+                            className=" rounded-2xl h-160 w-auto"
                         />
                     </div>
                 </div>
@@ -31,15 +31,44 @@ function Contato() {
                     <div className="flex flex-col items-start gap-6">
                         <h3 className="text-3xl font-bold text-gray-800">Entre em contato</h3>
                         <div className="flex items-center gap-6">
-                            <PhoneIcon size={48} />
-                            <WhatsappLogoIcon size={48} />
-                            <EnvelopeSimpleIcon size={48} />
+
+                            <ul className="text-sm space-y-1">
+                                <li className="flex flex-row">
+
+                                    <a 
+                                        href="tel:08007777777"
+                                        className="flex flex-row hover:underline"
+                                        aria-label="Ligar para 0800 777 7777"
+                                    >
+                                        <PhoneIcon size={28} /> 0800 777 7777
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a
+                                        href="https://wa.me/5511977777777"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="flex flex-row hover:underline"
+                                        aria-label="Abrir WhatsApp"
+                                    >
+                                        <WhatsappLogoIcon size={28} />(11) 97777-7777
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a
+                                        href="mailto:contato@carprotect.com.br"
+                                        className="flex flex-row hover:underline"
+                                        aria-label="Enviar email"
+                                    >
+                                        <EnvelopeSimpleIcon size={28} />contato@carprotect.com.br
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
-                        <span className="text-gray-700 font-medium mt-4 text-lg">
-                            contato@seudominio.com
-                        </span>
                     </div>
-                    
+
                     <div className=" w-full max-w-lg bg-white shadow-lg rounded-2xl p-8">
                         <h2 className="text-3xl text-center mb-4 ">Formulario</h2>
                         <form className="flex flex-col space-y-4">
