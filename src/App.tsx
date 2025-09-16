1add645049100f056da57da1cd8b68de8fa818ff
viviane santos <vjsantos.viviane@gmail.com>
2025-09-16 09:37:18 -0300

    feat: implements dashboard-client page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import AuthPage from "./pages/authpages/AuthPages"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardClient from "./pages/dashboardclient/DashboardClient";
 
 function App() {
-
   return (
-    <>
-      < AuthPage />
-      
-    </>
-  )
+    // Adicionado: O BrowserRouter é necessário para que as rotas e links funcionem.
+    <BrowserRouter>
+      <Routes>
+        {/* Adicionado: A sua dashboard agora está em uma rota, como deve ser. */}
+        <Route path="/dashboard" element={<DashboardClient />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
